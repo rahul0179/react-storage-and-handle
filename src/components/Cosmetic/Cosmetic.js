ce23b7798a7a5bc25cf0ede8af9b54d2dcc29aba
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-09-16 08:56:44 +0600

    eventhandeler with id

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     //console.log(props)
-    const { name, price } = props.cosmatic
+    const { name, price, id } = props.cosmatic
+
+    const addToCart = (id) => {
+        console.log('items Add', id)
+    }
+    const addToCartWithParam = () => addToCart(id);
 
     return (
-        <div>
+        <div className='product'>
             <h1> Name:{name}</h1>
             <h3>Price:{price}</h3>
+            <p><small>it has id: {id}</small></p>
+            <button onClick={addToCartWithParam}>Add to cart</button>
+            <button onClick={() => addToCart(id)}>Add to cart:Short cart</button>
 
 
         </div>
